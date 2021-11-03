53d474d1e4b6042281ef5626930f18bc5d14a3b0
Sanket <sanketpatil1228@gmail.com>
2021-11-03 12:23:00 +0530

    Dark mode added in about

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,37 +4,42 @@ export default function TextForm(props) {
     const [text, setText] = useState("");
 
     const handleUpClick = () =>{
-        // console.log("Click Upper Case");
+        
         let newText = text.toUpperCase();
         setText(newText);
     }
 
     const handleLoClick = () =>{
-        // console.log("Click Upper Case");
+        
         let newText = text.toLowerCase();
         setText(newText);
     }
 
     const handleOnChange = (event) =>{
-        // console.log("Click change Case");
         setText(event.target.value);
     }
 
     const clear = () =>{
-        // console.log("Click change Case");
         let newText = "";
         setText(newText);
     }
 
+    const handleCopy = () =>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    
     return (
         <>
         <div className="container my-3">
             <h1>{props.heading}</h1>
-            <div className="form-group">
-                <textarea className="form-control" value={text} onChange={handleOnChange}  id="exampleFormControlTextarea1" rows="8"></textarea>
+            <div className="form-group my-2">
+                <textarea className="form-control" value={text} onChange={handleOnChange}  id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-dark" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-dark mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-dark mx-2" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-dark mx-2" onClick={clear}>Clear</button>
         </div>
 
